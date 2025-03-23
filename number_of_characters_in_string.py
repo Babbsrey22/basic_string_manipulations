3a9591c13730e7a0afa26e3bc01b25f815b3bc56
Eunice <reyes05eunice@gmail.com>
2025-03-23 17:11:56 +0800

    prints number of characters in string

--- number_of_characters_in_string.py
+++ number_of_characters_in_string.py
@@ -2,3 +2,5 @@
 sentence = input("Enter a sentence: ")
 
 # Print number of characters in string (including spaces)
+character_count = len(sentence)
+print("The number of characters in this sentence is: ", character_count)
